107075dffba85e43fe7a7fe10f8912677edb3199
Fatima zahra El bouaazzaoui <fael-bou@e2r7p3.1337.ma>
2022-07-03 12:08:19 +0100

    creating the github repository

---
+++ philo/main.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fael-bou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/01 13:51:58 by fael-bou          #+#    #+#             */
+/*   Updated: 2022/07/02 20:30:37 by fael-bou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "philosophers.h"
+
+void	create_philosophers()
+{
+
+}
+
+int main (int argc, char *argv[])
+{
+	(void)argc;
+	(void)argv;
+	test_args(int argc, char *argv[]);
+	create_philosophers()
+	printf("hi\n");
+	return 0;
+}

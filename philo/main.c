6e177f59b19ead19516385ca9a9b483aed99e2c0
Fatima-Zahra Elbouaazzaoui <elbouaazzaouitema@gmail.com>
2022-08-27 23:05:47 +0000

    fix: return 0 if they ate all meals

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: fael-bou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 13:51:58 by fael-bou          #+#    #+#             */
-/*   Updated: 2022/08/17 17:37:12 by fael-bou         ###   ########.fr       */
+/*   Updated: 2022/08/27 23:05:27 by fatimzehra       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,5 @@ int main (int ac, char *av[])
 		return (-1);
 	get_args(&ctx, ac, av);
 	philosophers = create_philosophers(&ctx);
-	if (check_dead_philo(philosophers))
-		return (1);
-	exit(0);
-	return 0;
+	return (check_dead_philo(philosophers));
 }
